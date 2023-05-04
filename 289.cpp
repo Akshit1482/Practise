8d0510bb4b792b36fdcf571885577daf65e4bd67
Akshit Gupta <126385467+Akshit1482@users.noreply.github.com>
2023-05-04 13:41:38 +0530

    Update 289.cpp

--- 289.cpp
+++ 289.cpp
@@ -3,26 +3,27 @@ class Solution
 	public:
 	    void solve( string s, vector<string>& ans, int i ){
 	        // base case
-	        if( i == s.length() - 1 ){
+	        if( i >= s.length()){
 	            ans.push_back( s );
 	            return;
 	        }
 	        
 	        for( int k = i; k<s.size(); k++ ){
-	            swap( s[k],s[i] );
-	            solve( s, ans, i+1 );
+	            swap( s[i],s[k] );  // ek swap khus se hoga, aur ek ek swap jitne bhi next element hain unke saath hoga
+	            solve( s, ans, i+1 );  // i+1 becaause next step mein aage wale index par bhi toh jaana hai
 	            
 	            // backtrack
-	            swap( s[k],s[i] );
+	            swap( s[i],s[k] );
 	        }
 	    }
 		vector<string>find_permutation(string S)
 		{
-		    vector<string>ans;
-		    solve( S, ans, 0 );
+		    vector<string>ans;  // creating the ans vecctor
+		    int index = 0;
+		    solve( S, ans, index );
 		    sort( ans.begin(), ans.end() );
 		    
-		    ans.erase( unique( ans.begin(), ans.end() ), ans.end() );  // removing all unique entries in the ans vector
+		    ans.erase( unique( ans.begin(), ans.end() ), ans.end() ); // removing all unique entries in the ans vector
 		    return ans;
 		    
 		}
