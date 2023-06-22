02f6e835271ac8bd7a34b2455c6fcf2281074e48
Akshit Gupta <126385467+Akshit1482@users.noreply.github.com>
2023-06-22 15:11:23 +0530

    Create 073.cpp

---
+++ 073.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+    public:
+        vector <int> search(string pat, string txt)
+        {
+            vector<int>result;
+            int n = txt.size() - pat.size();
+            
+            // now we traverse on txt
+            for( int i=0; i<=n; i++ ){
+                string temp = txt.substr(i, pat.size());
+                if( temp == pat ){
+                    result.push_back( i+1 );
+                }
+            }
+            if( result.size() == 0 ){
+                result.push_back(-1);
+            }
+            return result;
+        }
+     
+};
