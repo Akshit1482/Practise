7ed4867c5eeb96af6b06762986e4eebe9c021926
Akshit Gupta <126385467+Akshit1482@users.noreply.github.com>
2023-06-23 12:35:19 +0530

    Create 075.cpp

---
+++ 075.cpp
@@ -0,0 +1,18 @@
+string printSequence(string S)
+{
+    string str[] = { "2","22","222","3","33","333","4","44","444","5","55","555",
+                    "6","66","666","7","77","777","7777","8","88","888","9","99","999","9999"};
+    
+    string ans = "";
+    for( int i=0; i<S.length(); i++ ){
+        
+        if( S[i] == ' ' ){
+            ans += "0";
+        }
+        else{
+            int temp = S[i] - 'A';
+            ans += str[temp];
+        }
+    }
+    return ans;
+}
