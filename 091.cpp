e57d626209eeebb36431973f4351f55a2d66caf1
Akshit Gupta <126385467+Akshit1482@users.noreply.github.com>
2023-06-23 16:35:39 +0530

    Create 091.cpp

---
+++ 091.cpp
@@ -0,0 +1,22 @@
+class Solution{
+  public:
+    vector<vector<string> > Anagrams(vector<string>& string_list) {
+        // creating a map to store the anagrams frequency
+        map<string, vector<string>>m;
+        // loop to travel the input string
+        for( int i=0; i<string_list.size(); i++ ){
+            string s = string_list[i];
+            sort( s.begin(), s.end() );  // this will make all anagrms similaar
+            m[s].push_back( string_list[i] );
+        }
+        
+        // creating a matrix for returning
+        vector<vector<string>>ans;  // as this atrix will be the size of the map
+        
+        // we'll start traversing the map
+        for( auto i : m ){
+            ans.push_back( i.second );
+        }
+        return ans;
+    }
+};
